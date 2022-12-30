ff24ca65a426937ffc4d84b49b6145faeb62757e
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2022-12-30 02:13:34 -0700

    feat(home) : modals

--- src/UI/molecules/SidebarUser.jsx
+++ src/UI/molecules/SidebarUser.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import { users } from '../../helpers/api';
+import LoginModal from '../molecules/LoginModal';
+
 export default function SidebarUser() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-      <Button
-        onClick={() => {
-          users();
-        }}
-        variant='outlined'
-        sx={{}}
-      >
+      <LoginModal open={open} handleClose={handleClose}></LoginModal>
+      <Button onClick={handleOpen} variant='outlined' sx={{}}>
         Log In
       </Button>
     </Box>
--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,20 +1,6 @@
 import axios from 'axios';
-const url = 'localhost:4000/api';
-
-function body(method, data) {
-  return axios({
-    method: method,
-    url: url,
-    data: data,
-  });
-}
-const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+export const api = axios.create({
+  baseURL: 'http://localhost:4000/api/users/',
   timeout: 1000,
   headers: {},
 });
-
-export async function users() {
-  const response = await api.get('users');
-  console.log(response.data);
-}
