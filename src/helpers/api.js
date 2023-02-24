55470ca0462b30e7383fb36940be727991138523
erickdominguez <erickdomroli@gmail.com>
2023-02-23 18:33:57 -0600

    fix : headers

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,8 +4,7 @@ export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   timeout: 10000000,
   headers: {
-    'Access-Control-Allow-Origin: '*',
-    Accept: '*/*',
-    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': '*/*',
   },
 });
