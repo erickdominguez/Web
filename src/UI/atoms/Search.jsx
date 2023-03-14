349578051523a02473246e0bbc71e7e9ddd24c6f
Ernesto Cedano <ecedano@iunu.com>
2023-03-14 12:57:00 -0700

    added search feature frontend.

--- src/UI/atoms/Search.jsx
+++ src/UI/atoms/Search.jsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+import AlbumIcon from '@mui/icons-material/Album';
+import PersonIcon from '@mui/icons-material/Person';
+import { Autocomplete, Box, TextField } from '@mui/material';
+import { useEffect } from 'react';
+import { api } from '../../helpers/api';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -30,7 +36,7 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
   justifyContent: 'center',
 }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
+const StyledInputBase = styled(TextField)(({ theme }) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
@@ -47,13 +53,66 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+
+const iconResult = type => {
+  if (type === 'song'){
+    return <AudiotrackIcon/>
+  } else if (type === 'album'){
+    return <AlbumIcon/>
+  }
+  return <PersonIcon/>
+}
 export default function SearchAppBar() {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState();
+  const navigate = useNavigate();
+
+  const handleSearch = async search => {
+    if (search && search.length > 0) {
+      setLoading(true)
+      const {data} = await api.get(`/song/search?search=${search}`)
+      setResults(data)
+      setLoading(true)
+    }
+ 
+  }
+
+  useEffect(() => {
+ 
+  }, [])
+  const linkStyle = {
+    textDecoration: 'none'
+  };
   return (
-    <Search>
-      <SearchIconWrapper>
-        <SearchIcon />
-      </SearchIconWrapper>
-      <StyledInputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} />
-    </Search>
+    <Autocomplete
+    id="search"
+    sx={{ width: 250 }}
+    options={results}
+    loading={loading}
+    getOptionLabel={(option) => option.name || option.title}
+    groupBy={option => option.type}
+    loadingText='Buscando...'
+    disabledItemsFocusable={true}
+    renderOption={(props, option) => {
+      return (
+          <Box  {...props} onClick = {() => {
+            if (option.type === 'album') {
+              navigate(`${option.type}s/${option?._id}`)
+            }
+            
+            }} >
+            {iconResult(option.type)}
+            <p style={{fontSize: 11, marginLeft: 10}}>{`  ${option.name || option.title}`}</p>
+        </Box>
+       
+      )
+    }}
+    renderInput={(params) => (
+      <StyledInputBase  {...params}  placeholder='Search…' />
+    )}
+    onInputChange={(event, newInputValue) => {
+      handleSearch(newInputValue);
+    }}
+  />
   );
 }
